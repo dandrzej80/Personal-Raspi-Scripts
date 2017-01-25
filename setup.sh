0b44f47e638e55bcaf933777a4696dff828a34b9
dandrzej80 <dandrzej80@gmail.com>
2017-01-24 21:09:14 -0500

    Update setup.sh
    Miscellaneous Changes

--- setup.sh
+++ setup.sh
@@ -4,12 +4,19 @@
 #Enable SSH/VNC
 echo "" > /boot/ssh
 
+#Enable Boot To Console
+
 #Update/Upgrade Raspian
 sudo apt-get update
 sudo apt-get -y upgrade
 
 #Update Firmware
 sudo rpi-update
+
+#Customize File System
+
+#Run raspi-config
+sudo raspi-config
 
 #Reboot Pi
 sudo shutdown -r
