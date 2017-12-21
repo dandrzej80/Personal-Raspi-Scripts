a9915f85c586fdb6f6487283192b21534a4fb2ef
dandrzej80 <dandrzej80@gmail.com>
2017-12-21 13:15:45 -0500

    Organization Changes
    Changes made to have this basic setup to be used and pulled from more specific setup scripts.

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,18 +1,6 @@
 #!/bin/bash -e
 #General Setup Script For Raspian Install
 
-#Run raspi-config
-sudo raspi-config
-
-#Enable SSH/VNC
-#sudo echo "" > /boot/ssh
-
-#Enable Boot To Console
-
-#Edit Crontab File
-#Auto Reboot Nightly
-sudo ed -s /etc/crontab $'-1 r edit-crontab\nw\nq\n'
-
 #Update/Upgrade Raspian
 sudo apt-get update
 sudo apt-get -y upgrade
@@ -20,8 +8,23 @@ sudo apt-get -y upgrade
 #Update Firmware
 sudo rpi-update
 
+#Run raspi-config
+sudo raspi-config
+
+#Enable SSH/VNC
+#sudo echo "" > /boot/ssh
+sudo install tightvncserver
+
+#Install Github
+sudo apt-get install git
+
+#Enable Boot To Console
+
+#Edit Crontab File
+#Auto Reboot Nightly
+sudo ed -s /etc/crontab $'-1 r edit-crontab\nw\nq\n'
+
 #Customize File System
 
 #Reboot Pi
-sudo shutdown -r now
-
+#sudo shutdown -r now
